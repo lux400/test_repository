86a073a702456f875a005af1d094bb2b8479b2cd
vladimir.tsimbalenko <vladimir.tsimbalenko@alteryx.com>
2024-06-06 19:46:24 +0300

    Added two logs

--- src/sum.test.js
+++ src/sum.test.js
@@ -5,6 +5,8 @@ describe('test sum', () => {
 
   it('should return 5', () => {
     const result = sum(2, 3);
+    console.log(result);
+    console.log(result);
     expect(result).toEqual(5);
   })
 })
